f4a786ff3282bd2ed7158de385cc0cbbc3e62f27
mjkou <1844646307@qq.com>
2018-07-30 13:03:48 +0800

    add number test method

--- spring-app/src/test/java/com/thoughtworks/training/NumberProcessorTest.java
+++ spring-app/src/test/java/com/thoughtworks/training/NumberProcessorTest.java
@@ -2,11 +2,14 @@ package com.thoughtworks.training;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class NumberProcessorTest {
 
     @Test
-    public void shouldReturn135WhenInput12345() {
-
+    public void shouldFilterOdd() {
+        assertThat(new NumberProcessor().process("1 2 3 4 5"),is("1 3 5"));
     }
 
 
